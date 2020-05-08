4977ff1d80742c40baa327b6d62453a5eb34bb4c
guojunjie <121785021@qq.com>
2020-05-08 17:50:44 +0800

    in company 2

--- interview_app/models.py
+++ interview_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class InterviewQuestion(models.Model):
+    question = models.CharField(max_length=255, null=False, unique=True, verbose_name='面试题目')
+    answer = models.TextField(max_length=2000, null=True, verbose_name='面试题答案')
+    create_time = models.DateTimeField(auto_now_add=True, null=False, verbose_name='创建时间')
+
+    def __str__(self):
+        return self.question[:20]
+
+    class Meta:
+        db_table = 'interview_question'
+        ordering = ['-create_time']
+        verbose_name = '面试题'
+        verbose_name_plural = verbose_name
--- interview_app/views.py
+++ interview_app/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import generics
+
+from interview_app.models import InterviewQuestion
+from interview_app.serializers import InterviewSerializer
 
 
 def index(request):
     return render(request, 'interview_app/index.html')
+
+
+def add_question(request):
+    return render(request, 'interview_app/add_question.html')
+
+
+def delete_question(request):
+    return render(request, 'interview_app/delete_question.html')
+
+
+class InterviewView(generics.ListCreateAPIView):
+    queryset = InterviewQuestion.objects.all()
+    serializer_class = InterviewSerializer
+
+
+class InterviewDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = InterviewQuestion.objects.all()
+    serializer_class = InterviewSerializer
+
+
+
